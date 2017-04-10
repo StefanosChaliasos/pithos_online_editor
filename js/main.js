4615991eed18969e1ec9b5b9dfe0f6e36db022ce
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-04-10 21:01:07 +0300

    replace brak line take with \n

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.Preview').click(function(event) { 
        var filepath = (event.target.id);
        var example = readTextFile(filepath);
+       console.log(example);
+       example = example.replace(/(?:\r\n|\r|\n)/g, '<br />');
 	   $('#text').html(example);
        $('#myModal').modal('show');
        // alert(example)

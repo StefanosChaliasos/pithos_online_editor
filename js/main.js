83a41bc2adb5d0caab6ae4f49fb18b51d0697b44
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-04-15 01:15:40 +0300

    add modal header

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     $('.Preview').click(function(event) { 
        var filepath = (event.target.id);
+       var filename = filepath.replace(/^.*[\\\/]/, '');
        var example = readTextFile(filepath);
        // console.log(example.type); // undefined
        example = example.replace(/(?:\r\n|\r|\n)/g, '<br/>');
 	   $('#text').html(example);
+       $('.modal-title').html(filename);
        $('#myModal').modal('show');
-       // alert(example)
     });
 });

3ad9b920fadb03079548a5edd61858f571b65477
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-04-15 19:35:58 +0300

    add comments

--- js/quill.js
+++ js/quill.js
@@ -1,3 +1,4 @@
+// Create quill editor
 var quill = new Quill('#editor', {
     modules: {
             toolbar: '#toolbar'
@@ -7,25 +8,28 @@ var quill = new Quill('#editor', {
     theme: 'snow'
 });
 
+// Alert Saved when save clicked
 var customButton = document.querySelector('.save');
 customButton.addEventListener('click', function() {
-  alert('Saved!');
+    alert('Saved!');
 });
 
+// Set quill text from text file
 var filepath = findGetParameter("source");
 var example = readTextFile(filepath);
 quill.setText(example);
 
+// Set html title
 var filename = filepath.replace(/^.*[\\\/]/, '');
 document.getElementById("title").innerHTML = filename;
 
+// Add text value the quill value
 var form = document.querySelector('form');
 form.onsubmit = function() {
-  // Populate hidden form on submit
-  var about = document.querySelector('input[name=text]');
-  about.value = quill.getText(0, quill.getLength());
-  
-  alert(about.value);
-  
-  return false;
+    var about = document.querySelector('input[name=text]');
+    about.value = quill.getText(0, quill.getLength());
+
+    alert(about.value);
+
+    return false;
 };

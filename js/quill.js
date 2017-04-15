cf705b085c519d1f4e2eb2e02f3d88b0f3f39468
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-04-15 19:28:07 +0300

    add form element

--- js/quill.js
+++ js/quill.js
@@ -15,3 +15,17 @@ customButton.addEventListener('click', function() {
 var filepath = findGetParameter("source");
 var example = readTextFile(filepath);
 quill.setText(example);
+
+var filename = filepath.replace(/^.*[\\\/]/, '');
+document.getElementById("title").innerHTML = filename;
+
+var form = document.querySelector('form');
+form.onsubmit = function() {
+  // Populate hidden form on submit
+  var about = document.querySelector('input[name=text]');
+  about.value = quill.getText(0, quill.getLength());
+  
+  alert(about.value);
+  
+  return false;
+};
